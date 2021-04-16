513306a7b52ff7b16c5503584a20ac1eeed9db81
yangtzujou <yangtzujou@gmail.com>
2021-04-16 19:22:37 +0800

    Add existing file

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+  res.render("home");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+app.get("/experience", function(req, res) {
+  res.render("experience");
+})
+
+app.get("/course", function(req, res) {
+  res.render("course");
+})
+
+app.get("/collaboration", function(req, res) {
+  res.render("collaboration");
+})
+
+app.get("/signin", function(req, res) {
+  res.render("signin");
+})
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+});
